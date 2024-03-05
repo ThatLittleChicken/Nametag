b025e865432b8126b0850b68dbbec7d870ac95fe
genthoongyong <yonggenthoong@gmail.com>
2024-03-05 00:24:17 -0700

    Stores user

--- login.js
+++ login.js
@@ -1,26 +1,43 @@
 const signup = document.getElementById("signB");
 const login = document.getElementById("loginB");
 
+let loginMap = localStorage.getItem('loginMap') ?
+JSON.parse(localStorage.getItem('loginMap')) : [[]];
+
+
 signup.addEventListener("click", (e) => {
     e.preventDefault();
-    var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
 
     if (username == "" || password == "") {
         alert("Please enter username and password");
     } else {
-        alert("Successfully signed up, please login to continue");
+        if (loginMap.some((user) => user[0] == username)) {
+            alert("Username already exists");
+        } else {
+            loginMap.push([username, password]);
+            localStorage.setItem('loginMap', JSON.stringify(loginMap));
+            alert("Successfully signed up, please login to continue");
+        }
     }
 });
   
 login.addEventListener("click", (e) => {
     e.preventDefault();
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
     if (username == "" || password == "") {
         alert("Please enter username and password");
     } else {
-        alert("Successfully logged in");
+        if (!loginMap.some((user) => user[0] == username)) {
+            alert("Username does not exist");
+        } else if (!loginMap.some((user) => user[0] == username && user[1] == password)) {
+            alert("Incorrect password");
+        } else if (loginMap.some((user) => user[0] == username && user[1] == password)) {
+            alert("Successfully logged in");
+            window.location.replace("mytag.html");
+        }
     }
 });
