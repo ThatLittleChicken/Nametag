250f6ef1d2b6c931ff51b209d8e3053df6d42b31
genthoongyong <yonggenthoong@gmail.com>
2024-03-05 20:04:58 -0700

    Dynamic list

--- mytag.js
+++ mytag.js
@@ -2,7 +2,9 @@ let user = JSON.parse(sessionStorage.getItem('user')) ?? 'Not logged in';
 
 document.getElementById('user').textContent = user;
 
-let inputIdArray = ['name','phone','email','insta','fb','linkedin','others'];
+let inputIdArray = ['name','phone','email','insta','fb','x','linkedin','others'];
+
+const ul = document.querySelector('ul');
 
 inputIdArray.forEach((id) => {
     let el = document.getElementById(id);
@@ -13,13 +15,49 @@ inputIdArray.forEach((id) => {
 
 function updateField(el,id){
     id = id + "F";
-    if(el.value != null){
-        if(id == "phoneF"){
-            el.value = el.value.replace(/[^(0-9\(\)\-) ]/g, '');
-        }
-        if(id == "emailF"){
-            el.value = el.value.replace(/[^a-zA-Z0-9@.]/g, '');
-        }
-        document.getElementById(id).textContent = el.value;
+    let elF = document.getElementById(id);
+
+    if(id == "phoneF"){
+        el.value = el.value.replace(/[^(0-9\(\)\-) ]/g, '');
     }
+    if(id == "emailF"){
+        el.value = el.value.replace(/[^a-zA-Z0-9@.]/g, '');
+    }
+    if(elF && elF.tagName == "LI" && el.value == "") {
+        elF.remove();
+    } else if (!elF && el.value != ""){
+        addList(el.value,id);
+    }
+    elF.textContent = socialName(el.value,id);
+    
 }
+
+function socialName(text, id) {
+    switch(id){
+        case "instaF":
+            text.includes("Instagram:") ? text : text = `Instagram: ${text}`;
+            break;
+        case "fbF":
+            text.includes("Facebook:") ? text : text = `Facebook: ${text}`;
+            break;
+        case "xF":
+            text.includes("X/Twitter:") ? text : text = `X/Twitter: ${text}`;
+            break;
+        case "linkedinF":
+            text.includes("LinkedIn:") ? text : text = `LinkedIn: ${text}`;
+            break;
+        case "othersF":
+            text.includes("Others:") ? text : text = `Others: ${text}`;
+            break;
+        default:
+            break;
+    }
+    return text;
+}
+
+function addList(text,id){
+    const li = document.createElement('li')
+    li.textContent = text;
+    li.id = id;
+    ul.appendChild(li);
+  }
