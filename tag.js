e3bf7c483387569e0ae8a3d7b59befc39359b9e0
genthoongyong <yonggenthoong@gmail.com>
2024-03-06 15:02:24 -0700

    Added tag js

---
+++ tag.js
@@ -0,0 +1,50 @@
+//Data placeholder
+let user = 'john_doe';
+
+let allUserData = JSON.parse(localStorage.getItem('allUserData')) ?? {};
+allUserData[user] = {name: 'John Doe', phone: '800 123 4567', email: 'johndoe@gmail.com', insta: '@john_doe', fb: '', x: '', linkedin: '@john_doe', others: ''};
+
+//Show user data for tag
+for (let key in allUserData[user]) {
+    let elF = document.getElementById(`${key}F`);
+    elF.textContent = socialName(allUserData[user][key],`${key}F`);
+
+    if(elF && elF.tagName == "LI" && allUserData[user][key] == "") {
+        elF.remove();
+    } else if (!elF && allUserData[user][key] != ""){
+        addList(el.value,id);
+    }
+}
+
+function socialName(text, id) {
+    switch(id){
+        case "instaF":
+            text.includes("Instagram:") ? text : text = `Instagram: ${text}`;
+            break;
+        case "fbF":
+            text.includes("Facebook:") ? text : text = `Facebook: ${text}`;
+            break;
+        case "xF":
+            text.includes("X/Twitter:") ? text : text = `X/Twitter: ${text}`;
+            break;
+        case "linkedinF":
+            text.includes("LinkedIn:") ? text : text = `LinkedIn: ${text}`;
+            break;
+        case "othersF":
+            text.includes("Others:") ? text : text = `Others: ${text}`;
+            break;
+        default:
+            break;
+    }
+    return text;
+}
+
+//Add list to ul for socials
+const ul = document.querySelector('ul');
+
+function addList(text,id){
+    const li = document.createElement('li')
+    li.textContent = text;
+    li.id = id;
+    ul.appendChild(li);
+  }
