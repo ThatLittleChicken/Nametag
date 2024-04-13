6db419f6f94e824e113aa0e376b2eb41dab59cac
genthoongyong <yonggenthoong@gmail.com>
2024-04-13 03:00:38 -0600

    Ported login page

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,89 @@
 import React from 'react';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter, NavLink, Route, Routes, useNavigate } from 'react-router-dom';
+import { Login } from './login/login.jsx';
+import { MyTag } from './mytag/mytag.jsx';
+// import { Share } from './share/share';
+// import { Tag } from './tag/tag';
+import { AuthState } from './login/authState';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 function App() {
+    const [username, setUserName] = React.useState(localStorage.getItem('username') || '');
+    const currentAuthState = username ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+    const navigate = useNavigate();
 
+    function logout() {
+        fetch('/api/auth/logout', {
+            method: 'delete',
+        }).catch(() => {
+            // Logout failed. Assuming offline
+        }).then(() => {
+            localStorage.removeItem('username');
+            setAuthState(AuthState.Unauthenticated);
+            navigate('/');
+        });
+    }
+    
     return (
-        <BrowserRouter>
-            <div className='body'>App will display here</div>
-        </BrowserRouter>
+        <div className="d-flex flex-column">
+            <header className="border-bottom">
+                <nav className="navbar py-3">
+                    <NavLink to="/" className="navbar-brand ps-4">Name Tag</NavLink>
+                    <menu className="navbar-nav m-0 d-flex flex-row flex-grow-1 list-unstyled">
+                        {authState === AuthState.Authenticated && (
+                            <li className="nav-item px-2">
+                                <NavLink to="mytag" className="nav-link">My Tag</NavLink>
+                            </li>
+                        )}
+                        {authState === AuthState.Authenticated && (
+                            <li className="nav-item px-2">
+                                <NavLink to="share" className="nav-link">Share</NavLink>
+                            </li>
+                        )}
+                        {authState === AuthState.Authenticated && (
+                            <li className="nav-item px-2 ms-auto pe-4">
+                                <Button onClick={() => logout()} className="btn btn-secondary">Logout</Button>
+                            </li>
+                        )}
+                    </menu>
+                </nav>
+            </header>
+
+            <Routes>
+                <Route 
+                    exact
+                    path="/" 
+                    element={<Login 
+                        userName={username} 
+                        authState={authState} 
+                        onAuthChange={(username, authState) => {
+                            setUserName(username);
+                            setAuthState(authState);
+                        }}
+                    />} 
+                />
+                <Route 
+                    path="mytag" 
+                    element={<MyTag 
+                        userName={username} 
+                        authState={authState}
+                    />} 
+                />
+                {/* <Route path="share" element={<Share userName={username} />} />
+                <Route path="tag" element={<Tag />} /> */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+
+            <footer className="container-fluid border-top">
+                <div className="mx-4 py-3">
+                    <span>Gent Yong</span>
+                    <a href="https://github.com/ThatLittleChicken/Nametag" className="float-end">GitHub</a>
+                </div>
+            </footer>
+        </div>
     );
 }
 
---
+++ src/login/login.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthState } from './authState';
+import Button from 'react-bootstrap/Button';
+import './login.css';
+
+export function Login({ userName, authState, onAuthChange }) {
+    const [username, setUserName] = React.useState(userName);
+    const [password, setPassword] = React.useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        document.title = "Name Tag | Login";
+    }, []);
+
+    async function signUp(){
+        if (username == "" || password == "") {
+            alert("Please enter username and password");
+        } else if (password.length < 8) {
+            alert("Password must be at least 8 characters long");
+        } else {
+            const response = await fetch(`/api/auth/signup`, {
+                method: 'post',
+                body: JSON.stringify({ username: username, password: password }),
+                headers: {
+                  'Content-type': 'application/json; charset=UTF-8',
+                },
+            });
+    
+            if (response?.ok) {
+                localStorage.setItem('username', username);
+                alert("Successfully signed up");
+                onAuthChange(username, AuthState.Authenticated);
+                navigate('/mytag', { replace: true });
+            } else if (response?.status == 409) {
+                alert("Username already exists");
+            } else {
+                alert(`Error: ${response.json().msg}`);
+            }
+        }
+    }
+
+    async function login(){    
+        if (username == "" || password == "") {
+            alert("Please enter username and password");
+        } else {
+            const response = await fetch(`/api/auth/login`, {
+                method: 'post',
+                body: JSON.stringify({ username: username, password: password }),
+                headers: {
+                  'Content-type': 'application/json; charset=UTF-8',
+                },
+            });
+            
+            if (response.ok) {
+                localStorage.setItem('username', username);
+                alert("Successfully logged in");
+                onAuthChange(username, AuthState.Authenticated);
+                navigate('/mytag', { replace: true });
+            } else if (response.status == 401 || response.status == 404) {
+                alert("Incorrect password or username does not exist");
+            } else {
+                alert(`Error: ${response.json().msg}`);
+            }
+        }
+    }
+
+    return (
+        <main className="d-flex flex-column justify-content-around align-items-center">
+            <div className="border rounded p-5 m-5">
+                <h1 className="mb-2 pb-1 border-bottom">Name Tag</h1>
+                <form className="row justify-content-end g-3">
+                    <div className="col-md-6">
+                        <label for="username" className="form-label">Username</label>
+                        <input 
+                            type="text" 
+                            name="username" 
+                            id="username" 
+                            placeholder="Username" 
+                            value={username}
+                            onChange={(e) => setUserName(e.target.value)} 
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <label for="password" className="form-label">Password</label>
+                        <input 
+                            type="password" 
+                            name="password" 
+                            id="password" 
+                            placeholder="Password" 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col-md-auto text-end">
+                        <Button onClick={() => signUp()} className="btn btn-primary">Sign up</Button>
+                    </div>
+                    <div className="col-md-auto text-end">
+                        <Button onClick={() => login()} className="btn btn-primary">Login</Button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
+}
